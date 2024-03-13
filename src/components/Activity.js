4b1006cf7ca174486f0db5df1b7fa4c4f08ec9d2
Douglas <wdmdavison@icloud.com>
2024-03-13 14:40:18 -0500

    Activity list item component

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,1 +1,21 @@
 import './components.css'
+import React from 'react';
+
+const Activity = ({ activity }) => {
+    const dateString = activity.updatedAt;
+    const date = new Date(dateString);
+    const formattedDate = date.toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+    });
+    let listName = "Watch later";
+    if (activity.ActivityType === "favorite") {
+        listName = "favorites";
+    }
+    return (
+        <li className='activity-list-item'>
+            <p><span className='red-text'>{activity.user.username}</span> added <span className='red-text'>{activity.title.title}</span> to {listName} - {formattedDate}</p>
+        </li>
+    )
+}
